2f2a84c92ad0db178babc21b3d869cc5ee177a48
André Ribeiro <andrelr1999@hotmail.com>
2022-11-23 00:17:47 -0300

    Create unit_converter main file and test file

---
+++ testes_unit_converter.py
@@ -0,0 +1,12 @@
+import unittest
+from unit_converter import UnitConverter
+
+class TestUnitConverter(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def testStart(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
