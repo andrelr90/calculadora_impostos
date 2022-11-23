881644adbe44986953db453fee437bef1ee7ca20
André Ribeiro <andrelr1999@hotmail.com>
2022-11-23 17:31:07 -0300

    Create methods, test constructor

--- calculadora_imposto_renda.py
+++ calculadora_imposto_renda.py
@@ -1,18 +1,34 @@
-class CalculadoraImpostoDeRenda():
-    def __init__(self, salario_mensal):
-        self.salario_mensal = salario_mensal
+class CalculadoraImpostoDeRenda():    
+    LIMITES_E_PORCENTAGENS = [
+        (22847.76, 0),
+        (33919.80, 0.075),
+        (45012.60, 0.15),
+        (55.976,16, 0.225),
+        (float("inf"), 0.275)
+    ]
     
-    def get_faixa_imposto(self):
-        pass
+    def __init__(self, salario_mensal, ganha_decimo_terceiro):
+        if (salario_mensal < 0):
+            raise Exception("Salário deve ser maior que zero.")
 
-    def get_valor_imposto_mensal(self):
-        pass
+        self.salario_mensal = salario_mensal
+        self.ganha_decimo_terceiro = ganha_decimo_terceiro
+    
+    def get_porcentagem_imposto(self):
+        for limite, porcentagem in LIMITES_E_VALORES:
+            if self.salario_mensal <= limite:
+                return porcentagem
 
     def get_valor_imposto_anual(self):
-        pass
+        if self.ganha_decimo_terceiro:
+            salario_anual = 13 * self.salario_mensal
+        else:
+            salario_anual = 12 * self.salario_mensal
+        return self.get_porcentagem_imposto(salario_anual) * salario_anual
 
-    def calcular_impostos(self):
-        self.faixa = self.get_faixa_imposto()
-        self.valor = self.get_valor_imposto_mensal()
-        self.valorAnual = self.get_valor_imposto_anual()
+    def get_faixa_textual(self):
+        _, porcentagem = self.get_porcentagem_imposto()
+        if porcentagem == 0:
+            return "Isento"
+        return str(porcentagem*100) + '%'
     
--- testes_calculadora_imposto_renda.py
+++ testes_calculadora_imposto_renda.py
@@ -5,8 +5,14 @@ class TestCalculadoraImpostoDeRenda(unittest.TestCase):
     def setUp(self):
         pass
 
-    def testStart(self):
-        self.assertTrue(True)
+    def test_salario_menor_que_zero_deve_disparar_excecao(self):
+        self.assertRaises(Exception, CalculadoraImpostoDeRenda, -1, True)
+
+    def test_salario_igual_a_zero_nao_deve_disparar_excecao(self):
+        try:
+            calculadora = CalculadoraImpostoDeRenda(0, True)
+        except:
+            self.fail()
 
 if __name__ == '__main__':
     unittest.main()
